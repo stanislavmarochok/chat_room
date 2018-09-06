fa790a27dabb04c359b645cace40405081cf0975
Stas Marochok <ssssstas30@gmail.com>
2018-09-06 13:52:12 +0300

    Made the window of the chat room, next will bw done the server's part starting

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -12,4 +12,25 @@ public class Server extends JFrame{
 	private ObjectOutputStream outputStream;
 	private ObjectInputStream inputStream;
 	private ServerSocket serverSocket;
+	private Socket connection;
+	
+	public Server () {
+		super ("Серверная часть.");
+		userInputText = new JTextField();
+		userInputText.setEditable(false);
+		userInputText.addActionListener(
+				new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						sendMessage(e.getActionCommand());
+						userInputText.setText("");
+					}
+				}
+			);		
+		add(userInputText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		 add (new JScrollPane(chatWindow));
+		 setSize (300, 600);
+		 setVisible (true);
+	}
 }
