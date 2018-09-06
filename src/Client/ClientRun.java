9ddca70785a2c4282b848577cb378525dfcc7630
Stas Marochok <ssssstas30@gmail.com>
2018-09-06 16:26:44 +0300

    All the work is donegit add .))) It's working) Just need to start ClientRun file and ServerTest file and that will be a chat)

--- src/Client/ClientRun.java
+++ src/Client/ClientRun.java
@@ -1,7 +1,14 @@
 package Client;
 
 import Server.*;
+import javax.swing.JFrame;
 
 public class ClientRun {
-	
+	public static void main (String[] args)
+	{
+		Client evilNerd;
+		evilNerd = new Client("127.0.0.1");
+		evilNerd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		evilNerd.startClient();
+	}
 }
